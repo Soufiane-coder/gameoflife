3e0c79ec08aeeea0b4c2600edaf80a625b7aa311
soufiane-coder <soufiane.amimi@um5r.ac.ma>
2023-10-15 18:35:03 +0100

    fix dark mode

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth, getUserData } from "../lib/firebase";
 import UserLoader from "./layout/user-loader/user-loader.layout";
 import { selectCurrentRoutines } from "./redux/routines/routines.selector";
-import { createContext, createRef, useEffect , useState} from "react";
+import { createContext, createRef, useEffect, useState } from "react";
 import { getRoutines } from "../lib/firebase";
 import { setCurrentRoutines } from "./redux/routines/routines.actions";
 import { setCurrentUser } from "./redux/user/user.actions";
@@ -41,7 +41,7 @@ const App = ({ user, displayMode, routines, setCurrentRoutines, setCurrentUser }
         (async () => {
             if (user && !routines) {
                 // so we can make sure that our users firebase is the one that has been uploaded
-                if(!user.lastVisit?.toDate) return;
+                if (!user.lastVisit?.toDate) return;
                 setUserFromFirebaseLoading(false);
                 let routines = await getRoutines(user.uid)
                 routines = await initialProtocol(user, routines);
@@ -53,102 +53,107 @@ const App = ({ user, displayMode, routines, setCurrentRoutines, setCurrentUser }
     useEffect(() => {
         (async () => {
             if (userImp) {
-                try{
+                try {
                     const user = await getUserData(userImp)
                     setCurrentUser(user);
-                }catch(error){
+                } catch (error) {
                     console.log('notification: error accured', error)
                 }
             }
             // this condition avoid the initialization of userImp state so when userLoading is true we wait until it is done
-            else if(!userLoading){ 
+            else if (!userLoading) {
                 setUserFromFirebaseLoading(false);
             }
         })()
-    }, [userImp,userLoading])
+    }, [userImp, userLoading])
 
-    
+
     const style = {
         NotificationItem: { // Override the notification item
-          DefaultStyle: { // Applied to every notification, regardless of the notification level
-            fontSize: '2rem',
-            width: '40rem',
-          },
+            DefaultStyle: { // Applied to every notification, regardless of the notification level
+                fontSize: '2rem',
+                width: '40rem',
+            },
         },
         Title: {
             DefaultStyle: {
                 fontSize: '2rem',
             }
         },
-        MessageWrapper:{
+        MessageWrapper: {
             DefaultStyle: {
                 margin: '5px',
             }
         }
-      }
+    }
 
     return (
-        <MyContext.Provider id={displayMode} value={{notificationSystem}}>
-            {
-                user ? <PopupField /> : ''
-            }
+        <>
+            <div id={displayMode}>
+                <MyContext.Provider value={{ notificationSystem }}>
+                    {
+                        user ? <PopupField /> : ''
+                    }
 
-            {
-                 userLoading || userFromFirebaseLoading ? null : <NavigationBar />
-            }
-            
-            <NotificationSystem ref={notificationSystem} style={style} />
-            <Switch>
-                
-                <Route exact={true} path="/signin">
-                    {!user ? (
-                        <SignInAndSignUp />
-                    ) : (
-                        <Redirect to="/gameField" />
-                    )}
-                </Route>
-                <Route exact={true} path="/">
-                    <LandingPage />
-                </Route>
-                {
-                    userLoading || userFromFirebaseLoading ?
-                        <UserLoader /> :
-                        <>
-                            <UserBar user={user} />
-                            <Route exact={true} path="/clockView">
-                                {user ?
-                                    <ClockView />
-                                    :
-                                    <Redirect to="/signin" />}
-                            </Route>
-                            <Route exact={true} path='/gameField'>
-                                {user ?
-                                    <GameField />
-                                    :
-                                    <Redirect to="/signin" />}
-                            </Route>
-                            <Route exact={true} path="/statistics">
-                                {user ?
-                                    <>
-                                        <StatisticsPage />
-                                    </>
-                                    :
-                                    <Redirect to="/signin" />}
-                            </Route>
-                            <Route path="/settings" component={Setting} />
-                            <Route path='/road-map/:routineId'>
-                                {user ?
-                                    <RoadMap />
-                                    :
-                                    <Redirect to="/signin" />}
-                            </Route>
-                        </>
-                }
-                <Route exact={true} path="*">
-                    <div style={{ fontSize: "200px" }}>not found</div>
-                </Route>
-            </Switch>
-        </MyContext.Provider>
+                    {
+                        userLoading || userFromFirebaseLoading ? null : <NavigationBar />
+                    }
+
+                    <NotificationSystem ref={notificationSystem} style={style} />
+                    <Switch>
+
+                        <Route exact={true} path="/signin">
+                            {!user ? (
+                                <SignInAndSignUp />
+                            ) : (
+                                <Redirect to="/gameField" />
+                            )}
+                        </Route>
+                        <Route exact={true} path="/">
+                            <LandingPage />
+                        </Route>
+                        {
+                            userLoading || userFromFirebaseLoading ?
+                                <UserLoader /> :
+                                <>
+                                    <UserBar user={user} />
+                                    <Route exact={true} path="/clockView">
+                                        {user ?
+                                            <ClockView />
+                                            :
+                                            <Redirect to="/signin" />}
+                                    </Route>
+                                    <Route exact={true} path='/gameField'>
+                                        {user ?
+                                            <GameField />
+                                            :
+                                            <Redirect to="/signin" />}
+                                    </Route>
+                                    <Route exact={true} path="/statistics">
+                                        {user ?
+                                            <>
+                                                <StatisticsPage />
+                                            </>
+                                            :
+                                            <Redirect to="/signin" />}
+                                    </Route>
+                                    <Route path="/settings" component={Setting} />
+                                    <Route path='/road-map/:routineId'>
+                                        {user ?
+                                            <RoadMap />
+                                            :
+                                            <Redirect to="/signin" />}
+                                    </Route>
+                                </>
+                        }
+                        <Route exact={true} path="*">
+                            <div style={{ fontSize: "200px" }}>not found</div>
+                        </Route>
+                    </Switch>
+                </MyContext.Provider>
+            </div>
+        </>
+
     );
 }
 
