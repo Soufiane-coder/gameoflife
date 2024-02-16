3d4c733a64a29d3cde8641c255d4ca0117f5d786
soufiane-coder <soufiane.amimi@um5r.ac.ma>
2024-02-17 00:42:47 +0100

    testing google ads4

--- src/components/google-ad/google-ad.component.jsx
+++ src/components/google-ad/google-ad.component.jsx
@@ -1,14 +1,26 @@
 // AdComponent.js
-import React from 'react';
+import React, {useEffect} from 'react';
 import AdSense from 'react-adsense';
 
 const GoogleAd = () => {
+  useEffect(() => {
+
+    try {
+        (window.adsbygoogle = window.adsbygoogle || []).push({});
+    }
+
+    catch (e) {
+      console.error(e)
+    }
+
+},[]);
+
   return (
     <div>
       <h2>Content Above the Ad</h2>
       <AdSense.Google
         client="ca-pub-7175718056472732"
-        slot="title"
+        slot="1234567890"
         style={{ display: 'block' }}
         format="auto"
         responsive="true"
