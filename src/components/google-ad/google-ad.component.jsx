b6be94ca9d53bc2437425fa63d2d5109e2a07dab
soufiane-coder <soufiane.amimi@um5r.ac.ma>
2024-02-18 23:40:16 +0100

    Revert "testing google ads4"
    This reverts commit 3d4c733a64a29d3cde8641c255d4ca0117f5d786.

--- src/components/google-ad/google-ad.component.jsx
+++ src/components/google-ad/google-ad.component.jsx
@@ -1,26 +1,14 @@
 // AdComponent.js
-import React, {useEffect} from 'react';
+import React from 'react';
 import AdSense from 'react-adsense';
 
 const GoogleAd = () => {
-  useEffect(() => {
-
-    try {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
-    }
-
-    catch (e) {
-      console.error(e)
-    }
-
-},[]);
-
   return (
     <div>
       <h2>Content Above the Ad</h2>
       <AdSense.Google
         client="ca-pub-7175718056472732"
-        slot="1234567890"
+        slot="title"
         style={{ display: 'block' }}
         format="auto"
         responsive="true"
